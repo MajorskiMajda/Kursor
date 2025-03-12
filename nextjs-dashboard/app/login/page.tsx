7572babc7ae9f240554eda05a49fdaf298830a33
majdarina <majorskimajda98@icloud.com>
2025-03-12 01:31:36 +0100

    gg

--- nextjs-dashboard/app/login/page.tsx
+++ nextjs-dashboard/app/login/page.tsx
@@ -41,7 +41,7 @@ export default function LoginPage() {
     setError(""); // Clear previous errors
 
     try {
-      const response = await fetch(`http://localhost:5001/api/login`, {
+      const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/login`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password }), 
